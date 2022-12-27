0e47c6377a2a14f38a7ed56b01eb6cd29d556513
Sinan Demir <sinanndmrr@gmail.com>
2022-12-27 19:45:57 +0300

    Page component updated

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -4,7 +4,7 @@ const Page = ({ children, styles = '' }) => {
   return (
     <div
       className={classnames(
-        'text-center p-10 min-w-3/5 w-2/5 max-h-full mx-auto space-y-10 border-solid border-4 border-secondary overflow-hidden flex flex-col justify-start items-center bg-gray-100 rounded-3xl',
+        'text-center p-10 md:min-w-3/5 md:w-2/5 sm:w-auto max-h-full mx-auto space-y-10 border-solid border-4 border-secondary overflow-hidden flex flex-col justify-start items-center bg-gray-100 rounded-3xl',
         styles
       )}
     >

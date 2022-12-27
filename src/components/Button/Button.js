11dd1732f0f6a9f61310c80e4f06479d712beaca
Sinan Demir <sinanndmrr@gmail.com>
2022-12-27 14:19:34 +0300

    button component updated

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,16 +1,14 @@
 import { Link } from 'react-router-dom';
 import classnames from 'classnames';
 
-const Button = ({ children, to, styles, ...rest }) => {
+const Button = ({ children, styles, ...rest }) => {
   return (
-    <button
-      className={classnames( 'text-white h-12 w-3/4 bg-main rounded-2xl hover:text-main hover:bg-secondary hover:border-solid hover:border-2 hover:border-main', styles)}
+    <Link
       {...rest}
+      className={classnames('flex justify-center items-center text-white h-12 w-3/4 bg-main rounded-2xl hover:text-main hover:bg-secondary hover:border-solid hover:border-2 hover:border-main', styles)}
     >
-      <Link to={to}>
-        {children}
-      </Link>
-    </button>
+      {children}
+    </Link>
   )
 }
 export default Button;

a671eb883fc2154c59737dfcf7e463dd9e7c46f6
Sinan Demir <sinanndmrr@gmail.com>
2022-12-30 20:48:32 +0300

    changed "Toaster" location

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
 import store from 'store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <Toaster/>
         <App/>
       </BrowserRouter>
     </Provider>

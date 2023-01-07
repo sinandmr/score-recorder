b596eb5009912e0a567c1fd7ee85c34de646dff5
Sinan Demir <sinanndmrr@gmail.com>
2023-01-07 23:07:15 +0300

    Added localStorage and the option to restart the game

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,21 +1,45 @@
-import { setPenalty } from 'store/Game';
+import { setInitialState, setPenalty, setState } from 'store/Game';
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-hot-toast';
 import useGame from 'hooks/useGame';
 import { TAVLA } from 'constants/games';
+import { useEffect, useState } from 'react';
 
-const Modal = ({ index, t }) => {
+const Modal = ({ index, t, type = 'penalty' }) => {
   const dispatch = useDispatch();
-  const { game } = useGame();
+  const { game, teams } = useGame();
+  const [restart, setRestart] = useState(false);
+  const currentState = JSON.parse(window.localStorage.getItem('state'));
+
+  useEffect(() => {
+    if (!teams || teams.length === 0 || restart) {
+      dispatch(setState({ name: 'game', data: null }));
+      window.localStorage.removeItem('state');
+      setInitialState();
+    }
+  })
 
   const handleClick = () => {
+    if (type === 'restart') {
+      setRestart(true)
+      return toast.dismiss(t.id);
+    }
     dispatch(setPenalty({ index }));
     toast.dismiss(t.id);
+    currentState.teams[index].penalty += 1;
+    currentState.teams[index].total += game === TAVLA ? 2 : 101;
+    window.localStorage.setItem('state', JSON.stringify(currentState));
   }
 
   return (
     <div className={'flex flex-col'}>
-      <b>{game === TAVLA ? 'MARS (2 Puan)' : 'Ceza'} eklemeyi onaylıyor musunuz?</b>
+      <b>
+        {
+          type === 'penalty' ? `${game === TAVLA ? 'MARS (2 Puan)' : 'Ceza'} eklemeyi onaylıyor musunuz?` : (
+            'Oyunu bitirmek istediğinize emin misiniz?'
+          )
+        }
+      </b>
       <div className={'flex justify-around mt-3'}>
         <button
           className={'border-2 border-solid border-secondary p-2 rounded-xl'}
